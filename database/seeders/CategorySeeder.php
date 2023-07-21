9fea29099e0782f92eae6fc6abcd7c310db5d60a
Saadi7863 <104414382+Saadi7863@users.noreply.github.com>
2023-07-21 05:03:14 -0700

    seeder
    Co-Authored-By: Ishfaq Alvi <30772042+ishfaqalvi@users.noreply.github.com>

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+        	[
+                'branch_id'   => '2',
+                'image'       => 'upload/images/category/4.jpg',
+                'title'       => 'Ballroom',
+                'link'        => 'http://bradfoard.ourdevworks.com/',
+                'label'       => 'Main Branch',
+                'order'       => '1',
+                'status'      => 'Publish',
+            ],
+            [
+                'branch_id'   => '2',
+                'image'       => 'upload/images/category/7.jpg',
+                'title'       => 'Mehndi',
+                'link'        => 'http://bradfoard.ourdevworks.com/',
+                'label'       => 'Second Branch',
+                'order'       => '2',
+                'status'      => 'Publish',
+            ],
+            [
+                'branch_id'   => '3',
+                'image'       => 'upload/images/category/7.jpg',
+                'title'       => 'Baraat',
+                'link'        => 'http://bradfoard.ourdevworks.com/',
+                'label'       => 'Thired Branch',
+                'order'       => '3',
+                'status'      => 'Publish',  
+            ],
+            [
+                'branch_id'   => '3',
+                'image'       => 'upload/images/category/4.jpg',
+                'title'       => 'Waleema',
+                'link'        => 'http://bradfoard.ourdevworks.com/',
+                'label'       => 'Forth Branch',
+                'order'       => '4',
+                'status'      => 'Publish',
+            ],
+        ]);
+    }
+}
